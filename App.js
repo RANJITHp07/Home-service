aa24a034053c63ae907e9fc6149274ffd66d6e90
ranjith <ranjithp27122003@gmail.com>
2024-02-29 11:39:13 +0530

    feat: created login interface and utils folder

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to </Text>
-      <StatusBar style="auto" />
+    <View >
+      <Login/>
     </View>
   );
 }
--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -1,1 +1,56 @@
-rnf
+import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import color from '../../utils/color';
+
+export default function Login() {
+  return (
+    <View style={{alignItems:'center'}}>
+      <Image 
+        source={require('../../../assets/images/login.png')}
+        style={styles.loginImage}
+      />
+      <View style={styles.subContainer}>
+          <Text style={{fontSize:27,color: color.WHITE, textAlign:'center'}}>
+            Let's Find 
+            <Text style={{fontWeight:'bold'}}> Professional {'\n'} Cleaning and Repair </Text>
+            Services
+          </Text>
+          <Text style={{fontSize:17,color:color.WHITE, textAlign:'center', marginTop:20}}>
+            Best App to find services near you which deliver you a professional service.
+          </Text>
+          <TouchableOpacity style={styles.button} onPress={()=>console.log("helooo")}>
+          <Text style={{textAlign:'center',fontSize: 17, color:color.PRIMARY}}>
+          Let's Get Started
+          </Text>
+          </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  loginImage: {
+    width: 230,
+    height: 450,
+    marginTop: 70,
+    borderWidth: 4,
+    borderColor: color.BLACK,
+    borderRadius: 15
+  },
+subContainer:{
+    width:'100%',
+    height:"70%",
+    backgroundColor:color.PRIMARY,
+    marginTop:-20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 15
+},
+
+button:{
+    padding:15,
+    borderRadius: 99,
+    backgroundColor:color.WHITE,
+    marginTop: 40
+}
+});
